fa1753b411cc89add34a98a81e621b4abd7b8df7
schmidt_daniel <post@daniel-schmidt.com>
2022-06-23 10:40:43 +0200

    Cleanup

--- src/main/java/de/die_gfi/maze/Main.java
+++ src/main/java/de/die_gfi/maze/Main.java
@@ -3,14 +3,16 @@ package de.die_gfi.maze;
 import de.die_gfi.maze.pdf.MazePdfPrinter;
 import java.io.FileNotFoundException;
 
-
+// KEEP THIS FILE
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException
     {
         MazeGenerator mazeGenerator = new MazeGenerator();
         int[][] maze = mazeGenerator.generateMaze();
+
         mazeGenerator.printMaze(maze);
+
 
         MazePdfPrinter mazePdfPrinter = new MazePdfPrinter();
         mazePdfPrinter.printMaze(maze);
